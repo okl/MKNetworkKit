babc735ab7ad9301d9f14d766930dfee5f358965
jcruikshank <jcruikshank@onekingslane.com>
2012-10-23 14:12:32 -0700

    Squelched warnings

--- MKNetworkKit/MKNetworkOperation.m
+++ MKNetworkKit/MKNetworkOperation.m
@@ -525,7 +525,7 @@
 
 -(void) setUploadStream:(NSInputStream*) inputStream {
   
-#warning Method not tested yet.
+//#warning Method not tested yet.
   self.request.HTTPBodyStream = inputStream;
 }
 
@@ -954,7 +954,7 @@
       
       NSData *certData = [[NSData alloc] initWithContentsOfFile:self.clientCertificate];
       
-#warning method not implemented. Don't use client certicate authentication for now.
+//#warning method not implemented. Don't use client certicate authentication for now.
       SecIdentityRef myIdentity = nil;  // ???
       
       SecCertificateRef myCert = SecCertificateCreateWithData(NULL, (__bridge CFDataRef)certData);
@@ -968,7 +968,7 @@
       [challenge.sender useCredential:credential forAuthenticationChallenge:challenge];
     }
     else if (challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust) {
-#warning method not tested. proceed at your own risk
+//#warning method not tested. proceed at your own risk
       SecTrustRef serverTrust = [[challenge protectionSpace] serverTrust];
       SecTrustResultType result;
       SecTrustEvaluate(serverTrust, &result);
